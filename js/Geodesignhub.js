cf86fdd345d04900f1b7af275eb849e1ea3e759f
hrishiballal <hrishikeshballal@yahoo.com>
2023-01-24 12:50:01 +0000

    Added rewinding of diagram Geometry
    See RFC 7946: https://gis.stackexchange.com/questions/259944/polygons-and-multipolygons-should-follow-the-right-hand-rule

--- js/Geodesignhub.js
+++ js/Geodesignhub.js
@@ -62,6 +62,50 @@ const CLIMATE_ACTIONS = [
   }
 ];
 
+// Source: https://github.com/chris48s/geojson-rewind
+function rewind(gj, outer) {
+    var type = gj && gj.type, i;
+
+    if (type === 'FeatureCollection') {
+        for (i = 0; i < gj.features.length; i++) rewind(gj.features[i], outer);
+
+    } else if (type === 'GeometryCollection') {
+        for (i = 0; i < gj.geometries.length; i++) rewind(gj.geometries[i], outer);
+
+    } else if (type === 'Feature') {
+        rewind(gj.geometry, outer);
+
+    } else if (type === 'Polygon') {
+        rewindRings(gj.coordinates, outer);
+
+    } else if (type === 'MultiPolygon') {
+        for (i = 0; i < gj.coordinates.length; i++) rewindRings(gj.coordinates[i], outer);
+    }
+
+    return gj;
+}
+
+function rewindRings(rings, outer) {
+    if (rings.length === 0) return;
+
+    rewindRing(rings[0], outer);
+    for (var i = 1; i < rings.length; i++) {
+        rewindRing(rings[i], !outer);
+    }
+}
+
+function rewindRing(ring, dir) {
+    var area = 0, err = 0;
+    for (var i = 0, len = ring.length, j = len - 1; i < len; j = i++) {
+        var k = (ring[i][0] - ring[j][0]) * (ring[j][1] + ring[i][1]);
+        var m = area + k;
+        err += Math.abs(area) >= Math.abs(k) ? area - m + k : k - m + area;
+        area = m;
+    }
+    if (area + err >= 0 !== !!dir) ring.reverse();
+}
+
+
 /**
  * DIAGRAM READER
  */
@@ -76,7 +120,7 @@ import DiagramReader from './DiagramReader.js';
 //
 
 // TODO: Change this to live GDH URL.
-const API_URL = 'http://local.test:9000/api/v1';
+const API_URL = 'http://local.test:8000/api/v1';
 let _gdhNegotiatedDesignJSON = null;
 // JG //
 let _sourceScenarioFeaturesGeoJSON = null;
@@ -104,7 +148,13 @@ function ApiError(message, data, status) {
     return `${ this.message }\nResponse:\n${ isObject ? JSON.stringify(this.response, null, 2) : this.response }`;
   };
 }
-
+function wait(ms){
+  var start = new Date().getTime();
+  var end = start;
+  while(end < start + ms) {
+    end = new Date().getTime();
+ }
+}
 // API wrapper function
 const fetchResource = (path, userOptions = {}) => {
   // Define default options
@@ -475,6 +525,7 @@ function migrateGPLFeaturesAsDiagrams() {
   const gdhProjectID = document.getElementById("gdh-project-id").value;
 
   let source_diagrams_len = _diagramsGeoJSON.length;
+  console.log(_diagramsGeoJSON);
   for (let index = 0; index < source_diagrams_len; index++) {
     const current_diagram_feature = _diagramsGeoJSON[index];
     const gplSystem = current_diagram_feature.properties.system;
@@ -489,15 +540,18 @@ function migrateGPLFeaturesAsDiagrams() {
       gdhTagCodes =[gplTagCodes]
     }
     
-
     const gdhSystemID = gdhGPLSystemConverter(gplSystem);
     const gj = current_diagram_feature['geometry'];    
-    let gj_feature_collection = { "type": "FeatureCollection", "features": [current_diagram_feature] }
+    
     let geoJSONGeometryType = gj['type'].toLowerCase();
-
+    let rewound = rewind(gj, false);
+    current_diagram_feature['geometry'] = rewound;
+        
+    let gj_feature_collection = { "type": "FeatureCollection", "features": [current_diagram_feature] }
+    console.log(gj_feature_collection)
     if (geoJSONGeometryType == 'LineString') {
       geoJSONGeometryType = 'polyline';
-    }
+    } 
 
     var postJson = {"featuretype": geoJSONGeometryType, "description": "Test Migration", "geometry": gj_feature_collection};
 
@@ -528,6 +582,9 @@ function migrateGPLFeaturesAsDiagrams() {
             consoleElement.innerHTML = "Climate Actions successfully assigned to migrated Diagram.."
           })
             .catch(error => consoleElement.innerHTML = `<div>${error}</div>${consoleElement.innerHTML}`);
+            // Wait for some time to prevent GDH DDOS tooling
+            wait(250);
+            console.log("Submitted, waiting 250 ms..")
         }
         // Assign GPL Object ID 
 
@@ -536,6 +593,8 @@ function migrateGPLFeaturesAsDiagrams() {
             consoleElement.innerHTML = "Diagram notes updated.."
           })
             .catch(error => consoleElement.innerHTML = `<div>${error}</div>${consoleElement.innerHTML}`);
+            wait(100);
+            console.log("Waiting 100 ms..")
         }
 
       }).catch(error => {
